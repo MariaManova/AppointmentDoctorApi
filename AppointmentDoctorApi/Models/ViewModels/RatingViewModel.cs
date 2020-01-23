05822879618d204bd6c8d1975af1c55fdf12dee8
MariaManova <maffik1998@mail.com>
2020-01-23 13:00:48 +0300

    добавлена модель поучающая данные о поставленной оценке

---
+++ AppointmentDoctorApi/Models/ViewModels/RatingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentDoctorApi.Models.ViewModels
+{
+    public class RatingViewModel
+    {
+        public int TotalSumRating { get; set; }
+        public int NumRated { get; set; }
+        public double Rating { get; set; }
+        public int Assessment { get; set; }
+        public long Id_User { get; set; }
+    }
+}
