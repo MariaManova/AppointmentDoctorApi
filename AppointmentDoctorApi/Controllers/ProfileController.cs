a03d868260b2e85d0c36da8931f533ae99dbe786
MariaManova <maffik1998@mail.com>
2020-02-02 12:33:39 +0300

    добалено получение текущих и истории записей на прием

--- AppointmentDoctorApi/Controllers/ProfileController.cs
+++ AppointmentDoctorApi/Controllers/ProfileController.cs
@@ -21,41 +21,53 @@ namespace AppointmentDoctorApi.Controllers
             this.db = db;
         }
         // GET: api/Profile
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
         // GET: api/Profile/5
         [HttpGet("{id}")]
         public IActionResult Get(long id)
         {
-             var  pat = db.Patient.Include(p => p.MyAppointments).FirstOrDefault(p => p.Id == id);
-            var  us = db.User.Include(u => u.Photo).FirstOrDefault(u => u.Id == pat.Fk_User);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var  pat = db.Patient.Include(p => p.User).ThenInclude(u => u.Photo).Include(p => p.MyAppointments).ThenInclude(a => a.Doctor).ThenInclude(a => a.Speciality)
+                .Include(p => p.MyAppointments).ThenInclude(a => a.Doctor).ThenInclude(a => a.PlaceOfWork)
+                .Include(p => p.MyAppointments).ThenInclude(a => a.Doctor.User).ThenInclude(a => a.Photo).FirstOrDefault(p => p.Id == id);
+            if (pat == null)
+            {
+                return BadRequest();
+            }
+            var listApprs = pat.MyAppointments.Where(p => p.DateTimeReceipt > DateTime.Now).OrderBy(p => p.DateTimeReceipt).ToList();
+            var historyApprs = pat.MyAppointments.Where(p => p.DateTimeReceipt < DateTime.Now).OrderByDescending(p => p.DateTimeReceipt).ToList();
             return Ok(new
             {
                 patient = pat,
-                user = us
+                appointments = listApprs,
+                history = historyApprs,
             });
         }
 
         // POST: api/Profile
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
         // PUT: api/Profile/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
